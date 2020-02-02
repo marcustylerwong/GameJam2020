422fd999a803fc043682ecd59c382f590b149abb
marcustylerwong <marcustylerwong0@gmail.com>
2020-02-01 19:56:48 -0500

    Revert "Revert "added new room""
    This reverts commit a23591417dd7577ad25bbe8ab21c75c65c22a231.

---
+++ Assets/Scripts/checkGameState 2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GARGcheckGameState : MonoBehaviour
+{
+    private bool isFinished;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFinished = false;
+    }
+
+    void Update() {
+        mouseDrag[] allChildren = GetComponentsInChildren<mouseDrag>();
+        int numberLeft = allChildren.Length;
+        foreach (mouseDrag child in allChildren){
+            Transform transform = child.GetComponent<Transform>();
+            if (transform.position == child.originalPosition &&
+            transform.rotation == child.originalRotation) {
+                child.finish = true;
+            }
+            if (allChildren[0].finish == true && 
+            allChildren[1].finish == true && 
+            allChildren[2].finish == true && 
+            allChildren[3].finish == true && 
+            allChildren[4].finish == true) {
+                Debug.Log("ok");
+                isFinished = true;
+            }
+        }
+    }
+}
